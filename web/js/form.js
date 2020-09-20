4cfc082bd31492182321e581c140a789fdda47cf
Herivelton Costa <herivelton.PRo@gmail.com>
2020-09-20 18:05:12 -0300

    form.js ajustado

--- web/js/form.js
+++ web/js/form.js
@@ -1,17 +1,24 @@
 function outputCheckbox () {
   var inputGame =  document.querySelectorAll(".gameselect:checked");
   var inputGameArr = Array.from(inputGame);
+  //console.log(inputGame);
   console.log(inputGameArr);
+  //soma = 0;
+  s = 0;
 
-  soma = 0;
+  s = inputGameArr.length * 10;
 
+  console.log(s);
+
+  /*
   inputGameArr.forEach(element => {
     soma += 10; 
   });
+  */
 
   //document.getElementById('result').innerHTML = soma;
-  console.log(soma);
+  //console.log(soma);
 
-  return soma;
+  return s;
 
 }
