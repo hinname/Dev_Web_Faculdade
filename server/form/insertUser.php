a7e66018aba00493692d7df90d885bd6f13e2530
Herivelton Costa <herivelton.PRo@gmail.com>
2020-11-19 10:01:34 -0300

    inserindo usuario no DB

--- server/form/insertUser.php
+++ server/form/insertUser.php
@@ -1,13 +1,41 @@
 <?php
 
+function formataTelefone($numero){
+      $formata = substr($numero, 0, 2);
+      $formata_2 = substr($numero, 3, 5);
+      $formata_3 = substr($numero, 4, 4);
+      return "(".$formata.") " . $formata_2 . "-". $formata_3;
+}
+
 require_once('../acessoDB.php');
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$senha = $_POST['senha'];
-$sexo = $_POST['sexo'];
-$telefone = $_POST['telefone'];
+$senha = $_POST['senha1'];
+$sexo = $_POST['gender'];
+$telefone = formataTelefone($_POST['telefone']);
 $data_nasc = $_POST['datenasc'];
+
+
+$row = [
+      'nome' => $nome,
+      'email' => $email,
+      'senha' => $senha,
+      'sexo' => $sexo,
+      'telefone' => $telefone,
+      'data_nasc' => $data_nasc
+];
+
+
+$sql = "INSERT INTO usuarios SET 
+      nome=:nome, email=:email, senha=:senha, sexo=:sexo, telefone=:telefone, data_nasc=:data_nasc;";
+$status = $pdo->prepare($sql)->execute($row);
+
+if($status){
+      header('location: sucessoInsertUser.php');
+}else {
+      echo "Erro no Insert do usuario";
+}
 
 
 ?>
---
+++ server/form/sucessoInsertUser.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Sucesso no Cadastro</title>
+      <link rel="stylesheet" href="../../web/pages/global/css/global.css">
+</head>
+<body>
+      <center><h2>Cadastro Realizado com Sucesso!</h2></center>
+      <center><a 
+      href="../../web/pages/landingPage/html/index.html" 
+      style="background-color: #ba401b;
+      color: white;
+      padding: 1em 1.5em;
+      text-decoration: none;
+      text-transform: uppercase;
+      border-radius: 5%;"
+      >
+      Clique aqui para voltar para inicio
+      </a>
+      </center>
+</body>
+</html>
