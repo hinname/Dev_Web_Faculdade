7957b749b52d5bc0aa1c36aa7e5b6902f6c39186
Herivelton Costa <herivelton.PRo@gmail.com>
2020-11-20 21:38:05 -0300

    banco de dados atualizado

--- torneio_games_web.sql
+++ torneio_games_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Nov-2020 às 18:28
+-- Tempo de geração: 21-Nov-2020 às 01:36
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.3.23
 
@@ -48,6 +48,16 @@ CREATE TABLE `desenvolvedora` (
   `id_func` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `desenvolvedora`
+--
+
+INSERT INTO `desenvolvedora` (`id`, `nome`, `id_func`) VALUES
+(1, 'Capcom', 1),
+(2, 'Riot Games', 1),
+(3, 'Electronic Arts', 1),
+(4, 'Blizzard', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -82,6 +92,16 @@ CREATE TABLE `jogos` (
   `id_func` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `jogos`
+--
+
+INSERT INTO `jogos` (`id`, `nome`, `id_dev`, `id_func`) VALUES
+(1, 'Street Fighter V', 1, 1),
+(2, 'League of Legends', 2, 1),
+(3, 'Hearthstone', 4, 1),
+(4, 'Fifa', 3, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,13 +111,26 @@ CREATE TABLE `jogos` (
 CREATE TABLE `torneios` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `num_rounds` int(11) NOT NULL,
-  `status` varchar(100) NOT NULL,
+  `num_players` int(11) NOT NULL,
   `data_inicio` date NOT NULL,
   `data_termino` date NOT NULL,
   `id_jogo` int(11) NOT NULL,
   `id_func` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `torneios`
+--
+
+INSERT INTO `torneios` (`id`, `nome`, `num_players`, `data_inicio`, `data_termino`, `id_jogo`, `id_func`) VALUES
+(1, 'Torneio de Street Fighter V', 16, '2020-11-25', '2020-11-26', 1, 1),
+(2, 'Torneio de League of Legends', 16, '2020-12-01', '2020-12-01', 2, 1),
+(3, 'Torneio de Fifa', 16, '2020-12-09', '2020-12-09', 4, 1),
+(4, 'Torneio de Hearthstone', 16, '2020-12-10', '2020-12-10', 3, 1),
+(6, 'SuperWeekend Street Fighter V', 16, '2020-11-28', '2020-11-29', 1, 1),
+(7, 'SuperWeekend League of Legends', 16, '2020-12-05', '2020-12-05', 2, 1),
+(8, 'SuperWeekend Fifa', 16, '2020-12-12', '2020-12-12', 4, 1),
+(9, 'SuperWeekend Hearthstone', 16, '2020-12-19', '2020-12-19', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -114,6 +147,14 @@ CREATE TABLE `usuarios` (
   `telefone` varchar(15) NOT NULL,
   `data_nasc` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `sexo`, `telefone`, `data_nasc`) VALUES
+(1, 'Jose Souza Marques', 'souza.marques@hotmail.com', 'marques', 'M', '(21) 99999-9999', '2000-02-05'),
+(2, 'Lucas Machado', 'lucas.machado@gmail.com', 'lucas', 'M', '(21) 99999-9999', '1999-12-15');
 
 -- --------------------------------------------------------
 
@@ -217,7 +258,7 @@ ALTER TABLE `confrontos`
 -- AUTO_INCREMENT de tabela `desenvolvedora`
 --
 ALTER TABLE `desenvolvedora`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `funcionarios`
@@ -229,19 +270,19 @@ ALTER TABLE `funcionarios`
 -- AUTO_INCREMENT de tabela `jogos`
 --
 ALTER TABLE `jogos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `torneios`
 --
 ALTER TABLE `torneios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuario_confronto`
